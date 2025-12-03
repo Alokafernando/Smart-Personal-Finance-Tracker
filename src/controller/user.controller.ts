5c3eaf114af4524d387321a44513dd56f0f41215
Aloka_fernando <140930974+Alokafernando@users.noreply.github.com>
2025-12-03 21:44:45 +0530

    implement update user details

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -31,3 +31,23 @@ export const getUserById = async (req: Request, res: Response) => {
     }
 }
 
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params  
+    const updates = req.body    
+
+    const updatedUser = await User.findByIdAndUpdate(id, updates, {
+      new: true,       //update response eka penvnva
+      runValidators: true
+    }).select("-password")
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" })
+    }
+
+    res.status(200).json({ message: "User updated successfully", user: updatedUser })
+
+  } catch (err: any) {
+    res.status(500).json({ message: err.message || "Server error" })
+  }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express"
 import { authenticate } from "../middleware/auth"
 import { requireRole } from "../middleware/role"
 import { UserRole } from "../model/user.model"
-import { getAllUsers, getUserById } from "../controller/user.controller"
+import { getAllUsers, getUserById, updateUser } from "../controller/user.controller"
 
 const router = Router()
 
@@ -10,7 +10,7 @@ const ADMIN = [UserRole.ADMIN]
 
 router.get("/", authenticate, requireRole(ADMIN), getAllUsers) // GET /api/v1/user — admin: list users
 router.get("/:id", authenticate, requireRole(ADMIN), getUserById) // GET /api/v1/user/:id — admin: get user details
-//router.put("/:id", authenticate, requireRole([UserRole.ADMIN]), updateUser) // PUT /api/v1/user/:id — admin: update user
+router.put("/:id", authenticate, requireRole(ADMIN), updateUser) // PUT /api/v1/user/:id — admin: update user
 //router.delete( "/:id", authenticate, requireRole([UserRole.ADMIN]), deleteUser) // DELETE /api/v1/user/:id — admin: delete user
 
 export default router
