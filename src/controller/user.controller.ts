3b54fe74395408c332a49f6c5a6631db0b8118ac
Aloka_fernando <140930974+Alokafernando@users.noreply.github.com>
2025-12-03 21:14:36 +0530

    implement get user by id

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,17 +2,32 @@ import { Request, Response } from "express"
 import { User } from "../model/user.model"
 
 export const getAllUsers = async (req: Request, res: Response) => {
-  try {
-    const users = await User.find().select("-password") // hide password
+    try {
+        const users = await User.find().select("-password") // hide password
 
-    if (users.length === 0) {
-      return res.status(404).json({ message: "No users found" })
+        if (users.length === 0) {
+            return res.status(404).json({ message: "No users found" })
+        }
+
+        res.status(200).json({ count: users.length, users })
+    } catch (err: any) {
+        res.status(500).json({ message: err.message || "Server error" })
     }
-
-    res.status(200).json({ count: users.length, users})
-  } catch (err: any) {
-      res.status(500).json({ message: err.message || "Server error" })
-  }
 }
 
+export const getUserById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+
+        const user = await User.findById(id).select("-password")
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+
+        res.status(200).json({ user })
+    } catch (err: any) {
+        res.status(500).json({ message: err.message || "Server error" })
+    }
+}
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -9,7 +9,7 @@ const router = Router()
 const ADMIN = [UserRole.ADMIN]
 
 router.get("/", authenticate, requireRole(ADMIN), getAllUsers) // GET /api/v1/user — admin: list users
-//router.get("/:id", authenticate, requireRole(ADMIN), getUserById) // GET /api/v1/user/:id — admin: get user details
+router.get("/:id", authenticate, requireRole(ADMIN), getUserById) // GET /api/v1/user/:id — admin: get user details
 //router.put("/:id", authenticate, requireRole([UserRole.ADMIN]), updateUser) // PUT /api/v1/user/:id — admin: update user
 //router.delete( "/:id", authenticate, requireRole([UserRole.ADMIN]), deleteUser) // DELETE /api/v1/user/:id — admin: delete user
 
