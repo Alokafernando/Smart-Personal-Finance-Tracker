6d2a2601db57829ec333fd9cd4204ee9b3ce1b1c
Aloka_fernando <140930974+Alokafernando@users.noreply.github.com>
2025-12-06 15:01:54 +0530

    implement category model

---
+++ src/model/category.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Schema } from "mongoose"
+
+export enum CategoryType {
+    INCOME = "INCOME",
+    EXPENSE = "EXPENSE",
+}
+
+export interface ICategory extends Document {
+    _id: mongoose.Types.ObjectId
+    user_id: mongoose.Types.ObjectId | null  
+    name: string
+    icon: string
+    color: string
+    type: CategoryType
+    is_default: boolean
+    created_at: Date
+    updated_at: Date
+}
+
+const categorySchema = new Schema<ICategory>(
+    {
+        user_id: { type: Schema.Types.ObjectId, ref: "User",  default: null },
+        name: { type: String,  required: true,  trim: true, },
+        icon: { type: String, required: true,  default: "Tag"  },
+        color: { type: String,  required: true, default: "#4D96FF"  },
+        type: { 
+            type: String,
+            enum: Object.values(CategoryType), 
+            required: true 
+        },
+        is_default: { type: Boolean,  default: false },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+export const Category = mongoose.model<ICategory>( "Category", categorySchema )
