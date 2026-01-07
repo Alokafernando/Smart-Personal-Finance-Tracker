5bb70fb614c35b37644ef7f3daa4988f0afb0a1b
Alokafernando <buddhikaaloka2005@gmail.com>
2026-01-07 15:41:17 +0530

    test

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,8 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://smart-personal-finance-tracker-fe.vercel.app"],
+    // origin: ["http://localhost:5173", "https://smart-personal-finance-tracker-fe.vercel.app"],
+      origin: "https://smart-personal-finance-tracker-fe.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
